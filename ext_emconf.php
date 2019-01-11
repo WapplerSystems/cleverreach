acedfea56765e9ccd49f85eb644e4ca576c28d2c
Daniel Hoffmann <d.hoffmann@mac.com>
2019-01-11 14:17:06 +0100

    Raised Core-Dependency to 9.5
    - tested finisher with form
    - tested finisher with powermail

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.4',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '7.6.0-9.5.0',
             'php' => '7.0.0-7.2.99',
         ],
         'conflicts' => [],
