1138857c454070501e817f19a3898cd55fa0fd28
Dennis Metz <17856901+scriptserv@users.noreply.github.com>
2019-02-27 10:19:12 +0100

    Load Powermail PageTsConfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TypoScript/TsConfig/Page/powermail.tsconfig">'
+);
