d02ee0773ed00ce99d529c059a25e5d6a45f7d9a
Sven Wappler <info@wappler.systems>
2020-05-25 18:30:16 +0200

    Updated dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,11 +10,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'author_company' => 'WapplerSystems',
-    'version' => '0.1.6',
+    'version' => '0.1.7',
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.0-9.5.99',
-            'php' => '7.0.0-7.2.99',
+            'php' => '7.0.0-7.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
