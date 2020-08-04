b90156d81c83ae0b5ebd53964284334445c7e111
Sven Wappler <info@wappler.systems>
2020-08-04 16:17:23 +0200

    powermail tsconfig moved

--- ext_tables.php
+++ ext_tables.php
@@ -3,9 +3,5 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-//# Add page TSConfig
-$pageTsConfig = '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:cleverreach/Configuration/TypoScript/TSconfig/Page/powermail.tsconfig">';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($pageTsConfig);
-
 
 
