57567efdd3f6b03627451e7b3de4635177da6a19
Sven Wappler <info@wappler.systems>
2021-03-17 06:01:36 +0100

    Updated composer.json and fixed variable types.

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,11 +10,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'author_company' => 'WapplerSystems',
-    'version' => '0.1.8-dev',
+    'version' => '0.1.8',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
-            'php' => '7.0.0-7.4.99',
+            'php' => '7.3.0-7.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
