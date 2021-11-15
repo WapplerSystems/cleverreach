d017023c2c5648e53c4da4a478f6a5d6c97805b4
Thomas Helmrich <thomas@gigabit.de>
2021-11-15 13:56:18 +0100

    urlencode mail for special chars

--- Classes/CleverReach/Api.php
+++ Classes/CleverReach/Api.php
@@ -220,7 +220,7 @@ class Api
         }
 
         try {
-            $this->rest->get('/groups.json/' . $groupId . '/receivers/' . $id);
+            $this->rest->get('/groups.json/' . $groupId . '/receivers/' . urlencode($id));
             return true;
         } catch (\Exception $ex) {
             if ($ex->getCode() !== 404) {
@@ -245,7 +245,7 @@ class Api
         }
 
         try {
-            $return = $this->rest->get('/groups.json/' . $groupId . '/receivers/' . $id);
+            $return = $this->rest->get('/groups.json/' . $groupId . '/receivers/' . urlencode($id));
             return Receiver::createInstance($return);
         } catch (\Exception $ex) {
             if ($ex->getCode() !== 404) {
